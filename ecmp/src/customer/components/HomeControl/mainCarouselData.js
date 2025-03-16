b3955ce6b0097bfde79e8c25f4536466f23a40b5
Ashhish-h <kushwaha.ashish2611@gmail.com>
2025-03-16 12:21:12 +0530

    refactor MainCarousel to use external data source for items

---
+++ ecmp/src/customer/components/HomeControl/mainCarouselData.js
@@ -0,0 +1,19 @@
+export const mainCarouselData=[
+    {
+        image: "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/2/_/2_27.jpg",
+        path: "/women/clothing/lengha_choli"
+    },
+
+    {
+        image: "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/8/_/8_16.jpg",
+        path:"/women/clothing/women_dress"
+    },
+    {
+        image: "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/7/_/7_15.jpg",
+        path:"/women/clothing/women_dress"
+    },
+    {
+        image: "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/6/_/6_13.jpg",
+        path:"/women/clothing/women_saree"
+    },
+    ]
