be4b17a926b47e3142dfe4fb34ab5600d24db38a
Ashhish-h <kushwaha.ashish2611@gmail.com>
2025-03-21 16:24:32 +0530

    refactor ProductCardCarousel and ProductCards components for improved functionality and responsiveness add buttons for sliding products

--- ecmp/src/customer/components/ProductCardCarousel/ProductCardCarousel.jsx
+++ ecmp/src/customer/components/ProductCardCarousel/ProductCardCarousel.jsx
@@ -1,32 +1,78 @@
 import React from "react";
-import AliceCarousel from 'react-alice-carousel';
+import AliceCarousel from "react-alice-carousel";
 import ProductCards from "../ProductCards/ProductCards";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import { Button } from "@mui/material";
+import { useState } from "react";
+import { mens_kurta } from "../../../data/men/men_kurta";
 
 export default function ProductCardCarousel() {
+  const [activeSlide, setActiveSlide] = useState(0);
+  const responsive = {
+    // for responsiveness here 0, 720, 1024 are screen sizes and items are number of items to be displayed.
+    0: { items: 1 },
+    720: { items: 3 },
+    1024: { items: 5.5 },
+  };
 
-    const responsive = {
-        0: { items: 1 },
-        720: { items: 3 },
-        1024: { items: 5 },
-    };
+  const slidePrev = () => setActiveSlide(activeSlide - 1);
+  const slideNext = () => setActiveSlide(activeSlide + 1);
+  const syncSlides = ({ item }) => setActiveSlide(item);
+  const items = mens_kurta.map((item) => (
+    <ProductCards product={item}/>
+  ));
 
-    const items = [1, 1, 1, 1, 1].map((item) => <ProductCards /> )
-
-    return (
-        <div>
-
-            <AliceCarousel
-                mouseTracking
-                items={items}
-                // responsive={responsive}
-                disableButtonsControls
-                autoPlay
-                autoPlayInterval={1500}
-                infinite
-                controlsStrategy="alternate"
-                responsive={responsive}
+  return (
+    <div className="relative">
+      <div className="relative p-5">
+        <AliceCarousel
+          mouseTracking
+          items={items}
+          disableButtonsControls
+          disableDotsControls
+          responsive={responsive}
+          onSlideChanged={syncSlides}
+          activeSlide={activeSlide}
+        />
+        {activeSlide !== items.length - 5 && (
+          <Button
+            variant="contained"
+            className="z-50"
+            onClick={slideNext}
+            sx={{
+              position: "absolute",
+              top: "8rem",
+              right: "0rem",
+              transform: "translateX(50%) rotate(90deg)",
+              bgcolor: "white",
+            }}
+            aria-label="next"
+          >
+            <KeyboardArrowLeftIcon
+              sx={{ transform: "rotate(90deg)", color: "black" }}
             />
-
-        </div>
-    )
+          </Button>
+        )}
+        {activeSlide !== 0 && (
+          <Button
+            variant="contained"
+            className="z-50"
+            onClick={slidePrev}
+            sx={{
+              position: "absolute",
+              top: "8rem",
+              left: "0rem",
+              transform: "translateX(-50%) rotate(-90deg)",
+              bgcolor: "white",
+            }}
+            aria-label="next"
+          >
+            <KeyboardArrowLeftIcon
+              sx={{ transform: "rotate(90deg)", color: "black" }}
+            />
+          </Button>
+        )}
+      </div>
+    </div>
+  );
 }
--- ecmp/src/customer/pages/HomePage/HomePage.jsx
+++ ecmp/src/customer/pages/HomePage/HomePage.jsx
@@ -9,7 +9,11 @@ export default function HomePage() {
         <div>
             <MainCarousel />
 
-            <div>
+            <div className="space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10">
+                <ProductCardCarousel />
+                <ProductCardCarousel />
+                <ProductCardCarousel />
+                <ProductCardCarousel />
                 <ProductCardCarousel />
             </div>
         </div>
