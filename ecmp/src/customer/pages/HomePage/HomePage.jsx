a5d551a879de153be3b0dbbd09f8ca6ea579e838
Ashhish-h <kushwaha.ashish2611@gmail.com>
2025-03-24 10:51:31 +0530

    add Footer component and update HomePage to include multiple ProductCardCarousels with section names

--- ecmp/src/customer/pages/HomePage/HomePage.jsx
+++ ecmp/src/customer/pages/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import MainCarousel from "../../components/HomeControl/MainCarousel";
 import ProductCards from "../../components/ProductCards/ProductCards";
 import ProductCardCarousel from "../../components/ProductCardCarousel/ProductCardCarousel";
 import { mens_kurta } from "../../../data/men/men_kurta";
+import Footer from "../../components/Footer/Footer";
 
 
 export default function HomePage() {
@@ -11,12 +12,16 @@ export default function HomePage() {
             <MainCarousel />
 
             <div className="space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10">
-                <ProductCardCarousel data={mens_kurta}/>
-                <ProductCardCarousel data={mens_kurta}/>
-                <ProductCardCarousel data={mens_kurta}/>
-                <ProductCardCarousel data={mens_kurta}/>
-                <ProductCardCarousel data={mens_kurta}/>
+                <ProductCardCarousel data={mens_kurta} sectionName={"mens'kurta"}/>
+                <ProductCardCarousel data={mens_kurta} sectionName={"mens'shoes"}/>
+                <ProductCardCarousel data={mens_kurta} sectionName={"men's watch"}/>
+                <ProductCardCarousel data={mens_kurta} sectionName={"Men's Jeans"}/>
+                <ProductCardCarousel data={mens_kurta} sectionName={"Men's trouser"}/>
                
+            </div>
+
+            <div>
+                <Footer />
             </div>
         </div>
     );
